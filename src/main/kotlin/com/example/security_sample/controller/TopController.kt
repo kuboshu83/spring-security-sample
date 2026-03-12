382695b84d261b019c8f87412d370fc9bc2cef9c
kuboshu83 <dot.sohkubo@gmail.com>
2026-03-13 08:33:36 +0900

    トップページ追加した

---
+++ src/main/kotlin/com/example/security_sample/controller/TopController.kt
@@ -0,0 +1,16 @@
+package com.example.security_sample.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+
+@RequestMapping("/")
+@Controller
+class TopController {
+    @GetMapping
+    fun getTopPage(model: Model): String {
+        model.addAttribute("name", "akira")
+        return "index"
+    }
+}
