ee80d2e4f69dac5cd972b2558a15dbc3cd06eae1
kuboshu83 <dot.sohkubo@gmail.com>
2026-03-13 07:16:20 +0900

    first commit

---
+++ src/main/kotlin/com/example/security_sample/SecuritySampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.security_sample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SecuritySampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<SecuritySampleApplication>(*args)
+}
