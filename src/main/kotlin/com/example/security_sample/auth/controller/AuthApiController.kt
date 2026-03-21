171101b5370652dc2a3f64973775305f9f137a04
kuboshu83 <dot.sohkubo@gmail.com>
2026-03-21 10:34:54 +0900

    ログ出力修正

--- src/main/kotlin/com/example/security_sample/auth/controller/AuthApiController.kt
+++ src/main/kotlin/com/example/security_sample/auth/controller/AuthApiController.kt
@@ -2,6 +2,7 @@ package com.example.security_sample.auth.controller
 
 import com.example.security_sample.auth.domain.AuthUserRole
 import com.example.security_sample.auth.service.AuthUserService
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
 
@@ -32,11 +33,13 @@ class ApiAuthController(
 class AuthController(
     private val service: AuthUserService
 ) {
+    private val logger = LoggerFactory.getLogger(AuthController::class.simpleName)
+
     @PostMapping("/new")
     fun createUser(
         @ModelAttribute user: NewUserDTO,
     ): String {
-        println("${user.name}, ${user.password}, ${user.role}")
+        logger.info("Create new user: name=${user.name} role=${user.role}")
         val role = AuthUserRole.from(user.role)
             ?: throw RuntimeException("不明なロール: ${user.role}")
         service.createUser(user.name, user.password, role)
